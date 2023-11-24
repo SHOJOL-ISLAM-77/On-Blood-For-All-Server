ddd82234c708cda2160d428b10f9f84dc687aa9e
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2023-11-24 13:23:09 +0600

    create stracture for backend

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const port = process.env.PORT || 7000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6ptml9.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {  
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+    
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Blood running");
+});
+
+app.listen(port, () => {
+  console.log(`Blood donation is running on port ${port}`);
+});
